1ba8e7822eb6928f9a15b8e7cf2939a183d16da7
webmisfit <76257594+webmisfit@users.noreply.github.com>
2022-11-06 00:00:34 +0600

    2end completed

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path')
 const filePath = path.join(__dirname, '..', '01-read-file', 'text.txt');
-fs.readFile(foobar, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   console.log(data);
 });
---
+++ 02-write-file/index.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path')
+const readline = require('readline');
+const filePath = path.join(__dirname, '..', '02-write-file', 'text.txt');
+
+
+function write(filePath) {
+    const writableStream = fs.createWriteStream(filePath, {flags:'a'});
+
+    writableStream.on('error',  (error) => {
+        console.log(error);
+    });
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Введите текст: '
+    });
+
+    rl.prompt();
+      
+    rl.on('line', (line) => {
+        switch (line.trim()) {
+            case 'exit':
+                rl.close();
+                break;
+            default:
+                sentence = line + '\n'
+                writableStream.write(sentence);
+                rl.prompt();
+                break;
+        }
+    }).on('close', () => {
+        writableStream.end();
+        writableStream.on('finish', () => {
+            console.log(`\nДо скорой встречи`);
+        })
+        setTimeout(() => {
+            process.exit(0);
+        }, 100);
+    });
+}
+
+write(filePath)
+
