fb3090764b7b9180c91632107094bdaf63adb119
webmisfit <76257594+webmisfit@users.noreply.github.com>
2022-11-09 11:28:20 +0600

    completed 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, '..', '03-files-in-folder/secret-folder');
+
+fs.readdir(filePath, { withFileTypes: true }, function (err, fileNames) {
+  for (let fileName of fileNames) {
+    if (!fs.statSync(filePath + '/' + fileName.name).isDirectory()) {
+      let x = path.parse(filePath + '/' + fileName.name);
+      let size = (fs.statSync(filePath + '/' + fileName.name).size / 1024).toFixed(3);
+      console.log(`${x.name} - ${x.ext.slice(1)} - ${size}кб`);
+    }
+  }
+});
